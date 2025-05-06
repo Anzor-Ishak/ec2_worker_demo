4cb18b548186915cc0c5322abe16ff11b63ff01e
Anzor Ishak <anzori@spacelift.io>
2025-05-06 15:47:19 -0400

    removed 2.3.5 module, and added 3.0.3

--- main.tf
+++ main.tf
@@ -13,23 +13,27 @@ provider "aws" {
 
 module "my_workerpool" {
   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v3.0.3"
-
-  configuration = <<-EOT
-    export SPACELIFT_TOKEN="${var.worker_pool_config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
-  EOT
-
-  min_size                   = 1
-  max_size                   = 5
-  worker_pool_id             = var.worker_pool_id
-  security_groups            = var.worker_pool_security_groups
-  vpc_subnets                = var.worker_pool_subnets
-  spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
-  spacelift_api_key_id       = var.spacelift_api_key_id
-  spacelift_api_key_secret   = var.spacelift_api_key_secret
   
+  secure_env_vars = {
+    SPACELIFT_TOKEN            = var.worker_pool_config
+    SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
+  }
+  configuration = <<EOF
+    export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
+  EOF
+
+  min_size          = 1
+  max_size          = 5
+  worker_pool_id    = var.worker_pool_id
+  security_groups   = var.worker_pool_security_groups
+  vpc_subnets       = var.worker_pool_subnets
+    
+    autoscaling_configuration = {
+    api_key_id       = var.spacelift_api_key_id
+    api_key_secret   = var.spacelift_api_key_secret
+    api_key_endpoint = var.spacelift_api_key_endpoint
+  }
 }
 
 #testing private worker pool 
 #add a worker2
-
