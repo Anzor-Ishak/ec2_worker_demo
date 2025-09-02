aacfa7b79503f1d5583e7df168c6657fd025b24b
Anzor Ishak <anzori@spacelift.io>
2025-09-02 12:13:27 -0400

    destroying components

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.96.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.96.0"
+#     }
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-2"
-}
+# provider "aws" {
+#   region = "us-east-2"
+# }
 
-module "my_workerpool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v3.0.3"
+# module "my_workerpool" {
+#   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v3.0.3"
   
-  secure_env_vars = {
-    SPACELIFT_TOKEN            = var.worker_pool_config
-    SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
-  }
-  configuration = <<EOF
-    export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
+#   secure_env_vars = {
+#     SPACELIFT_TOKEN            = var.worker_pool_config
+#     SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
+#   }
+#   configuration = <<EOF
+#     export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
 
-   # Private VCS mapping so the agent knows your local Bitbucket
-    export SPACELIFT_PRIVATEVCS_MAPPING_NAME_0=bbdc-test-pool
-    export SPACELIFT_PRIVATEVCS_MAPPING_BASE_ENDPOINT_0="https://7e8d-104-23-1-56.ngrok-free.app"
+#    # Private VCS mapping so the agent knows your local Bitbucket
+#     export SPACELIFT_PRIVATEVCS_MAPPING_NAME_0=bbdc-test-pool
+#     export SPACELIFT_PRIVATEVCS_MAPPING_BASE_ENDPOINT_0="https://7e8d-104-23-1-56.ngrok-free.app"
 
 
-  EOF
+#   EOF
 
-  min_size          = 1
-  max_size          = 5
-  worker_pool_id    = var.worker_pool_id
-  security_groups   = var.worker_pool_security_groups
-  vpc_subnets       = var.worker_pool_subnets
-  ec2_instance_type = var.ec2_instance_type
-}
+#   min_size          = 1
+#   max_size          = 5
+#   worker_pool_id    = var.worker_pool_id
+#   security_groups   = var.worker_pool_security_groups
+#   vpc_subnets       = var.worker_pool_subnets
+#   ec2_instance_type = var.ec2_instance_type
+# }
